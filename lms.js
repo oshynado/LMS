2f5c8d1391aff96146a0d4f9bb2e75d986428408
omar_nader <oshy.nado@gmail.com>
2025-03-07 21:14:11 +0200

    *add a set for the authors \n *add a book counter function \n *ad a count borrowed books \n *add function to ge most borrowed authors for books\n *add a report function to make a full report for books

--- lms.js
+++ lms.js
@@ -1,9 +1,53 @@
 let books = [];
+let authors = new Set();
 
 function addBook(title, author) {
   let id = books.length;
+  authors.add(author);
   let new_book = { id: id, title: title, author: author, isBorrowed: false };
   books.push(new_book);
+}
+
+//omar nader section
+
+function booksNumber() {
+  console.log(`Total Books: ${books.length}`);
+}
+
+function countBorrow() {
+  let borrowed = 0;
+  for (let book of books) {
+    if (book.isBorrowed) {
+      borrowed++;
+    }
+  }
+  console.log(`Books Borrowed: ${borrowed}`);
+  console.log(`Books Available: ${books.length - borrowed}`);
+}
+
+function most_borrow_author() {
+  let best_author = "",
+    n,
+    maxn = 0;
+  for (let author of authors) {
+    n = 0;
+    for (let book of books) {
+      if (author == book.author && book.isBorrowed) {
+        n++;
+      }
+    }
+    if (n > maxn) {
+      maxn = n;
+      best_author = author;
+    }
+  }
+  console.log(`Most Borrowed Author: ${best_author}`);
+}
+
+function report() {
+  booksNumber;
+  countBorrow;
+  most_borrow_author;
 }
 
 addBook("The Alchemist", "Paulo Coelho");
